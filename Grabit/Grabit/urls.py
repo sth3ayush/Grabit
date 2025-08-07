9e5b82bb4dcc8b7e6236e46cb01938e8d3cdbb5a
sth3ayush <sth3ayush001@gmail.com>
2025-08-07 15:55:38 +0545

    Update main urls

--- Grabit/Grabit/urls.py
+++ Grabit/Grabit/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('jet/', include('jet.urls', 'jet')),
     path('admin/', admin.site.urls),
+    path('', include('grabit_app.urls'))
 ]
